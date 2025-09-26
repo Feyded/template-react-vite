0d46c5218f0962c42ecde93b19bb405bce92e165
Dean Zaballero <126765808+Feyded@users.noreply.github.com>
2025-09-26 13:42:37 +0800

    feat: install reduxjs toolkit and add counter slice

--- src/main.tsx
+++ src/main.tsx
@@ -6,13 +6,17 @@ import { ThemeProvider } from "./components/theme-provider.tsx";
 import { Toaster } from "./components/ui/sonner.tsx";
 import ReactQueryProvider from "./components/query-client-provider.tsx";
 import AuthProvider from "./contexts/AuthContext.tsx";
+import { Provider } from "react-redux";
+import { store } from "./store/store.ts";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <ReactQueryProvider>
       <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
         <AuthProvider>
-          <App />
+          <Provider store={store}>
+            <App />
+          </Provider>
         </AuthProvider>
         <Toaster />
       </ThemeProvider>
